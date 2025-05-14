fd37743f460b1731276f0228aa098fb82afbb117
Vinicius <viniciusmsilva.ds@gmail.com>
2025-05-14 20:04:10 -0300

    Criando projeto asp.net core, banco fisico mysql e arquivo gitignore

---
+++ Script.sql
@@ -0,0 +1,31 @@
+create database bdEcommerce;
+
+use bdEcommerce;
+
+create table Usuario(
+Id int auto_increment,
+Nome varchar (50) not null,
+Email varchar (50) not null,
+Senha Varchar (50) not null,
+primary key (Id));
+
+create table Cliente(
+CodCli int auto_increment,
+NomeCli varchar (50) not null,
+TelCli varchar (20) not null,
+EmailCli varchar (50) not null,
+primary key (CodCli));
+
+create table Produto(
+CodProd int auto_increment,
+NomeProd varchar (50) not null,
+DescProd varchar (100) not null,
+QuantProd int not null,
+PrecoProd decimal (10,2) not null,
+primary key (CodProd));
+
+Select * from Usuario;
+Select * from Cliente;
+Select * from Produto;
+
+INSERT INTO Usuario (Nome, Email, Senha) VALUES ('admin', 'admin@gmail.com', '123456');
